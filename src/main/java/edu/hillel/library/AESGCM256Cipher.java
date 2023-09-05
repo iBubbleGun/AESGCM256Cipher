fb8c4511d4f2eb8c1da2f052b9020a086f2de76f
Tymur Kosiak <us1gav@gmail.com>
2023-09-05 15:38:47 +0300

    AESGCM256Cipher java library.

---
+++ src/main/java/edu/hillel/library/AESGCM256Cipher.java
@@ -0,0 +1,83 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2023 Tymur Kosiak ( https://github.com/iBubbleGun ).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package edu.hillel.library;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+/**
+ * @author Tymur Kosiak ( <a href="https://github.com/iBubbleGun">iBubbleGun GitHub</a> )
+ */
+public class AESGCM256Cipher {
+    private final int GCM_IV_LENGTH;
+    private final int GCM_TAG_LENGTH;
+    private final String ALGORITHM;
+
+    public AESGCM256Cipher() {
+        this.GCM_IV_LENGTH = 12;
+        this.GCM_TAG_LENGTH = 16;
+        this.ALGORITHM = "AES/GCM/NoPadding";
+    }
+
+    public String encrypt(String plaintext, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance(this.ALGORITHM);
+        byte[] iv = generateIV();
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(this.GCM_TAG_LENGTH * 8, iv);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, gcmParameterSpec);
+        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes(StandardCharsets.UTF_8));
+        byte[] combinedIVAndCipherText = new byte[this.GCM_IV_LENGTH + encryptedBytes.length];
+        System.arraycopy(iv, 0, combinedIVAndCipherText, 0, this.GCM_IV_LENGTH);
+        System.arraycopy(encryptedBytes, 0, combinedIVAndCipherText, this.GCM_IV_LENGTH, encryptedBytes.length);
+        return Base64.getEncoder().encodeToString(combinedIVAndCipherText);
+    }
+
+    public String decrypt(String encryptedText, SecretKey secretKey) throws Exception {
+        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] iv = new byte[this.GCM_IV_LENGTH];
+        System.arraycopy(encryptedBytes, 0, iv, 0, GCM_IV_LENGTH);
+        Cipher cipher = Cipher.getInstance(this.ALGORITHM);
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(this.GCM_TAG_LENGTH * 8, iv);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, gcmParameterSpec);
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes, this.GCM_IV_LENGTH, encryptedBytes.length - this.GCM_IV_LENGTH);
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
+    }
+
+    public SecretKey generateAESKey(final int KEY_SIZE) throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(KEY_SIZE);
+        return keyGenerator.generateKey();
+    }
+
+    private byte[] generateIV() {
+        byte[] iv = new byte[this.GCM_IV_LENGTH];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        return iv;
+    }
+}
